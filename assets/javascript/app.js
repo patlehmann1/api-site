25e21e44551ccd6004836bfb7076019df7fe72c5
Patrick Lehmann <patlehmann1@gmail.com>
2018-07-18 21:33:02 -0400

    removed unused return false

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,48 +1,42 @@
 var topicArray = ["Coffee", "Pizza", "Sushi", "Burritos", "Steak", "Seafood", "Barbeque", "Tacos", "Nachos", "Sweet Tea", "La Croix", "Perrier"];
 
-
-
-    function displayGifButtons(){
-        $("#button-display").empty(); 
-        for (var i = 0; i < topicArray.length; i++){
-            var gifButton = $("<button class='m-1'>");
-            gifButton.addClass("food btn btn-primary");
-            gifButton.attr("data-name", topicArray[i]);
-            gifButton.text(topicArray[i]);
-            $("#button-display").append(gifButton);
-        }
-
+function displayGifButtons() {
+    $("#button-display").empty();
+    for (var i = 0; i < topicArray.length; i++) {
+        var gifButton = $("<button class='m-1'>");
+        gifButton.addClass("food btn btn-primary");
+        gifButton.attr("data-name", topicArray[i]);
+        gifButton.text(topicArray[i]);
+        $("#button-display").append(gifButton);
     }
 
+}
 
-    function addNewButton(){
-        $("#addGif").on("click", function(){
+function addNewButton() {
+    $("#addGif").on("click", function () {
         var food = $("#newFood").val().trim();
-        if (food == ""){
-          return false;
+        if (food == "") {
+            return false;
         }
         topicArray.push(food);
-    
         displayGifButtons();
-        return false;
-        });
-    }
+    });
+}
 
-    function showGifs(){
-        var foodChoice = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + foodChoice + "&api_key=ahbCxqFdx1cR2Kf5VDqdp3djLKDGm81E&limit=10";
-        $.ajax({
-            url: queryURL,
-            method: 'GET'
-        })
-        .done(function(response) {
+function showGifs() {
+    var foodChoice = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + foodChoice + "&api_key=ahbCxqFdx1cR2Kf5VDqdp3djLKDGm81E&limit=10";
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    })
+        .done(function (response) {
             $("#gifs-go-here").empty();
             var results = response.data;
-            if (results == ""){
-              return false;
+            if (results == "") {
+                return false;
             }
-            for (var i=0; i< results.length; i++){
-    
+            for (var i = 0; i < results.length; i++) {
                 var gifDiv = $("<div>");
                 gifDiv.addClass("gifDiv col-sm-12 col-lg-3 border border-primary");
                 var gifRating = $("<p>").text("Rating: " + results[i].rating);
@@ -52,30 +46,29 @@ var topicArray = ["Coffee", "Pizza", "Sushi", "Burritos", "Steak", "Seafood", "B
                 gifDiv.append(gifTitle);
                 gifDiv.append(gifAdded);
                 var gifImage = $("<img>");
-                gifImage.attr("src", results[i].images.fixed_width_still.url); 
-                gifImage.attr("data-still",results[i].images.fixed_width_still.url); 
-                gifImage.attr("data-animate",results[i].images.fixed_width.url); 
-                gifImage.attr("data-state", "still"); 
+                gifImage.attr("src", results[i].images.fixed_width_still.url);
+                gifImage.attr("data-still", results[i].images.fixed_width_still.url);
+                gifImage.attr("data-animate", results[i].images.fixed_width.url);
+                gifImage.attr("data-state", "still");
                 gifImage.addClass("image");
                 gifDiv.append(gifImage);
                 $("#gifs-go-here").prepend(gifDiv);
             }
         });
 
-    }
+}
 
-    displayGifButtons();
-    addNewButton();
+displayGifButtons();
+addNewButton();
 
-    $(document).on("click", ".food", showGifs);
-    $(document).on("click", ".image", function(){
+$(document).on("click", ".food", showGifs);
+$(document).on("click", ".image", function () {
     var state = $(this).attr('data-state');
-    if ( state == 'still'){
+    if (state == 'still') {
         $(this).attr('src', $(this).data('animate'));
         $(this).attr('data-state', 'animate');
-    }else{
+    } else {
         $(this).attr('src', $(this).data('still'));
         $(this).attr('data-state', 'still');
     }
-
-    });
+});
